126cfeff94507143da0e45f0c1905773ab4accd8
Build Er <builder@macmini6.local>
2023-04-24 15:59:43 -0300

    Package version: 1.0.0-beta+20230424185248

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Media_PhotoLibrary",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_PhotoLibrary-1.0.0-beta+20230420132651.xcframework.zip",
-			checksum: "777de713431837e8e6d663b8dfcce9f03b7c033126a83bd374c96882d7b8584e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_PhotoLibrary-1.0.0-beta+20230424185248.xcframework.zip",
+			checksum: "3b03f5e0b702fc390b6c7a6345c19cb827d1e6f533eff8db1922d8a12ed55c9a"
 		)
 	]
 )
