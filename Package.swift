e18e5b68bf648ee5fd1b30eeab2fd410cd6b6233
genexusbot <builder@genexus.com>
2023-05-15 14:53:30 -0300

    Package version: 1.0.0-beta+20230515174624

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Media_PhotoLibrary",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_PhotoLibrary-1.0.0-beta+20230515153434.xcframework.zip",
-			checksum: "fb5b4396299b3085145296c0910b8ec22a86825320ded3e8863e46dcd6efb1da"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Media_PhotoLibrary-1.0.0-beta+20230515174624.xcframework.zip",
+			checksum: "ccfa9b2b4827caaa6afacc1be5b19c9710d6cafe616aa503582007435eba18a2"
 		)
 	]
 )
